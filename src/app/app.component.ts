810b2376ac8471e5ebd2cee26cbff7716d0e9a56
Ankit Jain <ankit@Ankits-MBP.fios-router.home>
2023-05-15 22:23:37 -0400

    fetching breeds list via api service in component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'dogs-breeds';
+  breeds: string[] = [];
+
+  constructor(private apiService: ApiService){ }
+  
+  ngOnInit(): void {
+    this.fetchDogBreeds();
+  }
+
+  fetchDogBreeds() {
+    this.apiService.getDogBreeds().subscribe((breeds)=> {
+      this.simplifyBreeds(breeds.message);
+    });
+  }
+
+  simplifyBreeds(breedsList: any[]) {
+    for (const breed in breedsList){
+      if ( breedsList[breed].length === 0 ) {
+        this.breeds.push(breed);
+      } else if (breedsList[breed].length > 0 ) {
+        breedsList[breed].forEach((sub_breed: string) => {
+          this.breeds.push(`${breed} - ${sub_breed}`)
+        })
+      }
+    }
+  }
+
 }
